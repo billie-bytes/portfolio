b6804cf1120985a2a2a2059641417bbb94ddb185
unknown <billiebaskarawibawa101@gmail.com>
2025-12-27 18:20:51 +0700

    feat:     - session system fully implemented     - some missing features of filesystems     implemented fix:     - fixed some errors involving wrong argument     orders when using utils (i got confused     by my own helper)

--- src/session.c
+++ src/session.c
@@ -1,11 +1,55 @@
 #include "session.h"
 
-Session* new_session(){
+Session sessions[MAX_SESSIONS];
+int session_count = 0;
+
+Session* alloc_session(){
     if(session_count>=MAX_SESSIONS) return (Session*)NULL;
     ++session_count;
     return &sessions[session_count-1];
 }
 
+void dealloc_session(){
+    if(session_count<=1) return; // Cannot dealloc the first session
+    if (sessions[session_count-1].is_active) {
+         if (session_count > 1) {
+             set_active_session(session_count - 2);
+         }
+    }
+    --session_count;
+}
+
 void session_initialize(){
-    
+    new_session();
+}
+
+
+Session* get_active_session(){
+    for(int i = 0; i<MAX_SESSIONS; i++){
+        if(sessions[i].is_active){
+            return &sessions[i];
+        }
+    }
+
+    sessions[0].is_active = TRUE;
+    return &sessions[0]; // default for an unknown error where no session is active
+}
+
+void set_active_session(int new_active_session_id){
+    for(int i = 0; i<MAX_SESSIONS; i++){
+        sessions[i].is_active = FALSE;
+    }
+    sessions[new_active_session_id].is_active = TRUE;
+}
+
+Session* new_session(){
+    Session* session = alloc_session();
+    if(session==NULL) return NULL;
+    session->session_id = session_count-1;
+    session->current_dir_id = 0;
+    session->last_exit_code = NO_EXIT_CODE;
+    session->is_active = TRUE;
+    set_active_session(session_count-1);
+
+    return session;
 }
--- src/session.h
+++ src/session.h
@@ -1,27 +1,66 @@
 #ifndef SESSION_H
 #define SESSION_H
 #define MAX_SESSIONS 4
+#define NO_EXIT_CODE 6969 //For newly created sessions
+
+#ifndef TRUE
+#define TRUE 1
+#endif
+
+#ifndef FALSE
+#define FALSE 0
+#endif
+
+#ifndef NULL
 #define NULL ((void*)0)
+#endif
 
 typedef struct Session {
-    int session_id;
-    int current_dir_id;
-    int last_exit_code;
+    int session_id; //Ranges from 0 to 3
+    int current_dir_id; //The offset of the directory inside the fs_node array
+    int last_exit_code; //Last exit code of a command run from this session
+    int is_active; //To know if this is the active terminal
 } Session;
 
-Session sessions[MAX_SESSIONS];
-int session_count = 0;
+extern Session sessions[MAX_SESSIONS];
+extern int session_count;
 
 /**
  * @brief creates a new session starting from root
  * 
  * @return Session* pointer of the new session
  */
-Session* new_session();
+Session* alloc_session();
+
+/**
+ * @brief deallocs a session starting from the last session made
+ * 
+ */
+void dealloc_session();
 
 /**
  * @brief Initializes the first working session
  */
 void session_initialize();
+
+/**
+ * @brief Get the active session
+ * 
+ * @return Returns the pointer of the active session. If for some unknown reason no session is active, defaults to the first session.
+ */
+Session* get_active_session();
+
+/**
+ * @brief Set the active session and deactivates every other session
+ * 
+ */
+void set_active_session(int new_active_session_id);
+
+/**
+ * @brief Create a new session (effectively a new terminal)
+ * 
+ */
+Session* new_session();
+
 
 #endif
